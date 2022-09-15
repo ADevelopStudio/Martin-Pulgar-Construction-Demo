9a6a3f83942f435ad6b15a4f084f5e2bfff9f1b8
Dmitrii Zverev <dima.newmail@gmail.com>
2022-09-15 14:14:59 +1000

    Initial Commit

---
+++ Martin Pulgar Construction Demo/Martin_Pulgar_Construction_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Martin_Pulgar_Construction_DemoApp.swift
+//  Martin Pulgar Construction Demo
+//
+//  Created by Dmitrii Zverev on 15/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct Martin_Pulgar_Construction_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
